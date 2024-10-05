4078526aed5ae0ef6e691d1e30e09f69626d54be
Phoenix-Ignited-Tech <cyber@phoenixignited.tech>
2024-10-04 20:13:03 -0600

    Add files via upload

---
+++ mytheme/admin/defaultconf/keycloak.v2/resources/assets/JsonFileUpload-C00oYgkL.js
@@ -0,0 +1,2 @@
+import{jsx as s}from"react/jsx-runtime";import{F as e}from"./FileUploadForm-Tqz1ZjTZ.js";import{L as i}from"./CodeEditor-BZUGCa3z.js";const m=({onChange:o,...n})=>{const a=r=>{try{o(JSON.parse(r))}catch{o({}),console.warn("Invalid json, ignoring value using {}")}};return s(e,{...n,language:i.json,extension:".json",onChange:a})};export{m as J};
+//# sourceMappingURL=JsonFileUpload-C00oYgkL.js.map
---
+++ mytheme/admin/defaultconf/keycloak.v2/resources/assets/isEqual-BKyM3i3m.js
@@ -0,0 +1,2 @@
+import{f8 as s}from"./main-hZIJIVx9.js";function o(a,r){return s(a,r)}export{o as i};
+//# sourceMappingURL=isEqual-BKyM3i3m.js.map
